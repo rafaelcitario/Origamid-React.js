0e69adce17d04ccfb7fc523d8175ea9808f69fc7
rafaelcitario <contato.rafaelgomes@outlook.com>
2023-06-09 03:11:50 -0300

    -a

---
+++ Aula04-2/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const App = () => {
+  const ramdom = Math.random();
+  const titulo = <h1>Este é um titulo</h1>;
+  let cor;
+  let ativo = false;
+
+  function showName() {
+    const name = 'Rafael';
+    return name;
+  }
+
+  const estilo = {
+    color: 'blue',
+    fontSize: '2rem',
+    border: '.2rem solid red',
+    padding: '1rem',
+    borderRadius: '999rem',
+  };
+
+  return (
+    <>
+      {showName()}
+      {titulo}
+      {ativo ? 'esta tivo' : 'não esta ativo'}
+      <p className={ativo ? (cor = 'green') : (cor = 'red')} style={estilo}>
+        {ramdom * 10}
+      </p>
+    </>
+  );
+};
+
+export default App;
